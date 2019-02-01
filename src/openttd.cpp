157c0bff012efa8691ecbb15c495d6f94d148f4d
Samu <dj_samu@hotmail.com>
2019-02-01 14:34:01 +0000

    Add: Client setting gui.start_spectator
    Activates a spectator slot in single player and initiate games as a spectator

--- src/openttd.cpp
+++ src/openttd.cpp
@@ -833,9 +833,9 @@ void HandleExitGameRequest()
  */
 static void OnStartGame(bool dedicated_server)
 {
-	/* Update the local company for a loaded game. It is either always
-	 * company #1 (eg 0) or in the case of a dedicated server a spectator */
-	SetLocalCompany(dedicated_server ? COMPANY_SPECTATOR : COMPANY_FIRST);
+	/* Update the local company for a loaded game. It is either always company #1 (eg 0)
+	 * or, in the case of starting as spectator or a dedicated server, a spectator. */
+	SetLocalCompany((dedicated_server || _settings_client.gui.start_spectator) ? COMPANY_SPECTATOR : COMPANY_FIRST);
 
 	/* Update the static game info to set the values from the new game. */
 	NetworkServerUpdateGameInfo();
@@ -854,18 +854,20 @@ static void MakeNewGameDone()
 		return;
 	}
 
-	/* Create a single company */
-	DoStartupNewCompany(false);
+	if (!_settings_client.gui.start_spectator) {
+		/* Create a single company */
+		DoStartupNewCompany(false);
 
-	Company *c = Company::Get(COMPANY_FIRST);
-	c->settings = _settings_client.company;
+		Company *c = Company::Get(COMPANY_FIRST);
+		c->settings = _settings_client.company;
 
-	/* Overwrite color from settings if needed
-	 * COLOUR_END corresponds to Random colour */
-	if (_settings_client.gui.starting_colour != COLOUR_END) {
-		c->colour = _settings_client.gui.starting_colour;
-		ResetCompanyLivery(c);
-		_company_colours[c->index] = (Colours)c->colour;
+		/* Overwrite color from settings if needed
+		 * COLOUR_END corresponds to Random colour */
+		if (_settings_client.gui.starting_colour != COLOUR_END) {
+			c->colour = _settings_client.gui.starting_colour;
+			ResetCompanyLivery(c);
+			_company_colours[c->index] = (Colours)c->colour;
+		}
 	}
 
 	OnStartGame(false);
@@ -1338,6 +1340,7 @@ void StateGameLoop()
 
 	Layouter::ReduceLineCache();
 
+	bool valid_local_company = _game_mode != GM_EDITOR && !_networking && _settings_client.gui.start_spectator && Company::IsValidID(_local_company);
 	if (_game_mode == GM_EDITOR) {
 		BasePersistentStorageArray::SwitchMode(PSM_ENTER_GAMELOOP);
 		RunTileLoop();
@@ -1385,6 +1388,10 @@ void StateGameLoop()
 	}
 
 	assert(IsLocalCompany());
+	if (valid_local_company && !Company::IsValidID(_local_company)) {
+		/* _local_company no longer exists due to bankruptcy. */
+		SetLocalCompany(COMPANY_SPECTATOR);
+	}
 }
 
 /**
