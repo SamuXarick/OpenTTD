abe401a49927d3844f39d2a8ae17693d735d5fc8
Samu <dj_samu@hotmail.com>
2019-02-28 02:12:18 +0000

    Add: Allow or disallow company level crossings as a game setting

--- src/saveload/saveload.h
+++ src/saveload/saveload.h
@@ -321,6 +321,7 @@ enum SaveLoadVersion : uint16 {
 	SLV_END_PATCHPACKS = 286,               ///< 286  Last known patchpack to use a version just above ours.
 
 	SLV_GS_INDUSTRY_CONTROL,                ///< 287  PR#7912 and PR#8115 GS industry control.
+	SLV_COMPANY_CROSSINGS_SETTING,          ///< 288  PR#7294 Add allow company level crossings setting.
 
 	SL_MAX_VERSION,                         ///< Highest possible saveload version
 };
