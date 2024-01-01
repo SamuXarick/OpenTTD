bf6592e5a264273f31e01f7aae723b65c07c3dce
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2024-01-01 17:17:10 +0000

    Change: Update group statistics earlier in AfterLoad
    This commit prefaces the next incoming set of commit changes in order to have the vehicle lists already generated as an attempt to prevent AfterLoad actions accessing them empty.

--- src/saveload/afterload.cpp
+++ src/saveload/afterload.cpp
@@ -1960,7 +1960,6 @@ bool AfterLoadGame()
 	}
 
 	if (IsSavegameVersionBefore(SLV_62)) {
-		GroupStatistics::UpdateAfterLoad(); // Ensure statistics pool is initialised before trying to delete vehicles
 		/* Remove all trams from savegames without tram support.
 		 * There would be trams without tram track under causing crashes sooner or later. */
 		for (RoadVehicle *v : RoadVehicle::Iterate()) {
@@ -3429,7 +3428,6 @@ void ReloadNewGRFData()
 	AfterLoadVehiclesPhase1(false);
 	AfterLoadVehiclesPhase2(false);
 	StartupEngines();
-	GroupStatistics::UpdateAfterLoad();
 	/* update station graphics */
 	AfterLoadStations();
 	/* Update company statistics. */
