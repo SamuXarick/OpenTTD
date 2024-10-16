f946dc00e9971e0f8310e635d490960321557b09
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2024-10-16 22:35:13 +0100

    Codechange: Remove TileAdd

--- src/viewport.cpp
+++ src/viewport.cpp
@@ -2943,8 +2943,8 @@ static int CalcHeightdiff(HighLightStyle style, uint distance, TileIndex start_t
 			/* Lambda to help calculating the height at one side of the line. */
 			auto get_height = [](auto &tile, auto &heightdiffs) {
 				return std::max(
-					TileHeight(TileAdd(tile, ToTileIndexDiff(heightdiffs.first))),
-					TileHeight(TileAdd(tile, ToTileIndexDiff(heightdiffs.second))));
+					TileHeight(tile + ToTileIndexDiff(heightdiffs.first)),
+					TileHeight(tile + ToTileIndexDiff(heightdiffs.second)));
 			};
 
 			/* Use lookup table for start-tile based on HighLightStyle direction */
